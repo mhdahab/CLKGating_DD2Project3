e9e0574fe3d66bc5fd518dd2fabc22159cbd645d
mhdahab <67865770+mhdahab@users.noreply.github.com>
2021-12-12 22:37:20 +0200

    Add files via upload

---
+++ GatingDD2.py
@@ -0,0 +1,208 @@
+import sys
+A1=[]
+X=[]
+S=[]
+gclk=[]
+def clkgating(inputfilename,clockname):
+    sky=0
+    wire=0
+    unique=0
+    wire_count=0
+    clkprinting=0
+    create_inv =0
+    reg = False
+    remove_nor=False
+    inputfile=open(inputfilename,"r+")
+    for h in inputfile:
+        if(h.find("sky130_fd_sc_hd__mux2")!=-1 or h.find("sky130_fd_sc_hd__a21oi")!=-1 ):
+            if(h.find("sky130_fd_sc_hd__a21oi")!=-1):
+                remove_nor=True
+                create_inv+=1
+            for g in inputfile:
+                if(g.find("[")!=-1):
+                    reg=True
+                    break
+                else:
+                    unique+=1
+                    break          
+    inputfile.close()
+    inputfile=open(inputfilename,"r+")
+    outputfile_name = inputfilename.replace(".gl.v",".gated.v")
+    outputfile= open(outputfile_name,'w+')
+    outputfile.write("`include \"primitives.v\""+'\n')
+    outputfile.write("`include \"sky130_hd.v\""+'\n')
+    if(unique ==0 or reg):
+        unique+=1
+    create_inv=create_inv*2
+    wire_count=unique
+    clkprinting=unique 
+    print_inv=create_inv
+    for i in inputfile:
+        line=i.split()
+        if((i.find("module")!=-1)and(i.find("endmodule")==-1)):
+            outputfile.write(i)
+            outputfile.write('\n')
+            name="_g"+clockname+str(wire)+"_ ;"
+            outputfile.write("  wire "+name+'\n')
+            wire_count-=1
+        elif((i.find("wire")!=-1)):
+            if(wire_count>0):
+                wire+=1
+                name="_g"+clockname+str(wire)+"_ ;"
+                wire_count-=1
+                outputfile.write("  wire "+name+'\n')
+            if(create_inv>0):
+                outputfile.write("  wire _"+str(create_inv)+"_ ;"+'\n')
+                create_inv-=1
+        elif(i.find("sky130_fd_sc_hd__mux2")!=-1):
+            for j in inputfile:
+                done=True
+                if(j.find(";")==-1):
+                    if(j.find("A1")!=-1):
+                        input=j.replace(".A1(","")
+                        input=input.replace("),","")
+                        input=input.split()
+                        A1.append(input[0]) 
+                    elif(j.find("X")!=-1):
+                        en=j.replace(".X(","")
+                        en=en.replace(")","")
+                        en=en.split()
+                        X.append(en) 
+                    elif(j.find("S")!=-1):
+                        input=j.replace(".S(","")
+                        input=input.replace("),","")
+                        input=input.split()
+                        S.append(input[0])
+                    done=False
+                if(done):
+                   break
+            if(clkprinting>(create_inv/2)):
+                sky+=1
+                outputfile.write("  sky130_fd_sc_hd__dlclkp_1 _"+str(sky)+"_ (" +'\n')
+                clkname="_g"+clockname+str(wire)+"_"
+                wire-=1
+                gclk.append(clkname)
+                enable=S.pop()
+                outputfile.write("   .GCLK("+clkname+")," +'\n')
+                outputfile.write("   .CLK(" +clockname+"),"+'\n')
+                outputfile.write("   .GATE("+enable+")" +'\n')
+                outputfile.write("  );" +'\n')
+                clkprinting-=1
+        elif(i.find("sky130_fd_sc_hd__a21oi")!=-1):
+            for j in inputfile:
+                done=True
+                if(j.find(";")==-1):
+                    if(j.find("A2")!=-1):
+                        input=j.replace(".A2(","")
+                        input=input.replace("),","")
+                        input=input.split()
+                        inv_name="_"+str(print_inv)+"_"
+                        print_inv-=1
+                        sky+=1
+                        outputfile.write("  sky130_fd_sc_hd__inv_"+str(sky)+"_ (" +'\n')
+                        outputfile.write("   .A("+input[0]+")," +'\n')
+                        outputfile.write("   .Y("+str(inv_name)+")" +'\n')
+                        outputfile.write("  );" +'\n')
+                        A1.append(inv_name) 
+                    elif(j.find("Y")!=-1):
+                        en=j.replace(".Y(","")
+                        en=en.replace(")","")
+                        en=en.split()
+                        X.append(en) 
+                    elif(j.find("A1")!=-1):
+                        input=j.replace(".A1(","")
+                        input=input.replace("),","")
+                        input=input.split()
+                        S.append(input[0])
+                    done=False
+                if(done):
+                   break
+            if(clkprinting>0 ):
+                sky+=1
+                outputfile.write("  sky130_fd_sc_hd__dlclkp_1 _"+str(sky)+"_ (" +'\n')
+                clkname="_g"+clockname+str(wire)+"_"
+                wire-=1
+                gclk.append(clkname)
+                enable=S.pop()
+                outputfile.write("   .GCLK("+clkname+")," +'\n')
+                outputfile.write("   .CLK(" +clockname+"),"+'\n')
+                outputfile.write("   .GATE("+enable+")" +'\n')
+                outputfile.write("  );" +'\n')
+                clkprinting-=1
+        elif((i.find("sky130_fd_sc_hd__dfxtp")!=-1)):
+            for j in inputfile:
+                done=True
+                if(j.find(";")==-1):
+                    if(j.find("Q")!=-1):
+                        Q=j.replace(".Q(","")
+                        Q=Q.replace(")","") 
+                        Q=Q.split()
+                    elif (j.find("D")!=-1):
+                        D=j.replace(".D(","")
+                        D=D.replace("),","") 
+                        D=D.split()
+                    done=False
+                if(done):
+                   break
+            line=str(i)
+            line=line.partition("(")[0]
+            mod_string=""
+            for z in range(len(line) - 3):
+                mod_string = mod_string + line[z]
+            sky+=1
+            outputfile.write("  "+mod_string+str(sky)+"_ (" +'\n') #print given size
+            if(len(gclk)>0):
+                gclk_name=gclk.pop()
+            #print ("A")
+            ind=X.index(D)
+            D_needed=A1[ind]
+            outputfile.write("   .CLK("+gclk_name+")," +'\n')
+            outputfile.write("   .D("+D_needed+")," +'\n')
+            outputfile.write("   .Q("+Q[0]+")" +'\n')
+            outputfile.write("  );" +'\n')
+
+        elif((i.find("/*")!=-1)):
+            pass
+        elif(remove_nor and (i.find("sky130_fd_sc_hd__nor")!=-1) ):
+            for j in inputfile:
+                done=True
+                if(j.find(";")==-1):
+                    pass
+                    done=False
+                if(done):
+                   break
+        else :
+            outputfile.write(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    inputfile.close()
+
+
+
+
+
+
+
+
+
+def main():
+    inputfilename =sys.argv[2]
+    clockname=sys.argv[1].replace("-","")
+    clkgating(inputfilename,clockname)
+    print ("Clock Gating Preformed")
+
+if __name__ == "__main__": 
+    main()
